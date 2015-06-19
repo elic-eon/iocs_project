7f27d7f69aeb5bf670d5b70e76ff3d2cbd70641c
ssuyi <ssuyi@cs.nctu.edu.tw>
2015-06-19 18:02:52 +0800

    commit from linux3

--- js/path.js
+++ js/path.js
@@ -1,3 +1,8 @@
+function writeMessage(message) {
+  text.setText(message);
+  layer.draw();
+}
+
 var stage = new Konva.Stage({
   container: 'container',
   width: 1600,
@@ -14,10 +19,35 @@ var layer = new Konva.Layer({
 
   });
 
+var text = new Konva.Text({
+  x: 1350,
+  y: 100,
+  fontFamily: 'Calibri',
+  fontSize: 36,
+  text: '',
+  fill: 'black'
+});
+
+layer.add(text);
+
 var ref = new Firebase('https://iocs-nctu.firebaseio.com/regions');
 ref.orderByKey().on("child_added", function(snapshot) {
-  var tmp = new Konva.Path(snapshot.val());
-  this.layer.add(tmp);
-  this.stage.add(layer);
-  console.log(snapshot.val());
+  var vals = snapshot.val();
+  vals.name = snapshot.key();
+  //var x = Math.random() * 800;
+  //var y = Math.random() * 400;
+  var node = new Konva.Path({
+	//x : x,
+	//y : y,
+	data : vals.data,
+	draggable : true,
+	fill : vals.fill,
+	stroke : vals.stroke,
+	strokeWidth : vals.strokeWidth,
+  });
+  node.on('mouseover', function() {
+    writeMessage(snapshot.key());
+  });
+  layer.add(node);
+  stage.add(layer);
 });
